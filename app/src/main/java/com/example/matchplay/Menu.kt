56d51aa8fa2821df1c8b161404ad93f8820415ac
Sebastian Romero <sebastiand.romeroc@utadeo.edu.co>
2024-09-29 22:29:30 -0500

    Fixed bugs

--- app/src/main/java/com/example/matchplay/Menu.kt
+++ app/src/main/java/com/example/matchplay/Menu.kt
@@ -1,6 +1,8 @@
 package com.example.matchplay
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,11 @@ class Menu : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_menu)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val button= findViewById<ImageButton>(R.id.perfil)
+
+        button.setOnClickListener{
+            val intent = Intent(this,Editar_Perfil::class.java)
+            startActivity(intent)
+        }
         }
     }
-}
