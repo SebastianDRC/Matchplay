c8c94ffa86c7fae1f5c2f960a0b7dcd3800ad048
Sebastian Romero <sebastiand.romeroc@utadeo.edu.co>
2024-10-17 11:00:57 -0500

    Fixed bugs

--- app/src/main/java/com/example/matchplay/Menu.kt
+++ app/src/main/java/com/example/matchplay/Menu.kt
@@ -5,18 +5,20 @@ import android.os.Bundle
 import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class Menu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_menu)
-        val button= findViewById<ImageButton>(R.id.perfil)
-
-        button.setOnClickListener{
-            val intent = Intent(this,Editar_Perfil::class.java)
+        val buttonperfil= findViewById<ImageButton>(R.id.perfilbotton)
+        buttonperfil.setOnClickListener{
+            val intent = Intent(this,Perfil::class.java)
+            startActivity(intent)
+        }
+        val buttoncomunidad= findViewById<ImageButton>(R.id.comunidad_nueva)
+        buttoncomunidad.setOnClickListener{
+            val intent = Intent(this,Crear_Comunidad::class.java)
             startActivity(intent)
         }
         }
--- app/src/main/java/com/example/matchplay/Perfil.kt
+++ app/src/main/java/com/example/matchplay/Perfil.kt
@@ -1,12 +1,14 @@
 package com.example.matchplay
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class perfil : AppCompatActivity() {
+class Perfil : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +18,11 @@ class perfil : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val button_cerrar_sesion= findViewById<ImageButton>(R.id.imageButtoncerrar_sesion)
+        button_cerrar_sesion.setOnClickListener{
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
