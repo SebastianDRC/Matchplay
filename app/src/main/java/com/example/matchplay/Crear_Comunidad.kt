a7ab1a80775ebdac9f98fa0a9289856483852726
anama <anamariaangelh@utadeo.edu.com>
2024-10-27 21:36:50 -0500

    Agrego perfil

--- app/src/main/java/com/example/matchplay/Crear_Comunidad.kt
+++ app/src/main/java/com/example/matchplay/Crear_Comunidad.kt
@@ -22,20 +22,8 @@ class Crear_Comunidad : AppCompatActivity() {
             val nameEditText = findViewById<EditText>(R.id.editTextText1)
             val sportEditText = findViewById<EditText>(R.id.editTextText2)
             val descriptionEditText = findViewById<EditText>(R.id.editTextText3)
-            val createButton = findViewById<Button>(R.id.button)
 
-            createButton.setOnClickListener {
-                val name = nameEditText.text.toString()
-                val sport = sportEditText.text.toString()
-                val description = descriptionEditText.text.toString()
-
-                if (name.isNotEmpty() && sport.isNotEmpty() && description.isNotEmpty()) {
-                    Toast.makeText(this, "Comunidad creada con éxito", Toast.LENGTH_SHORT).show()
-                    // Aquí puedes agregar lógica adicional para guardar la información o enviar a otro lugar
-                } else {
-                    Toast.makeText(this, "Por favor, completa todos los campos", Toast.LENGTH_SHORT).show()
                 }
             }
         }
-    }
-        }
+
